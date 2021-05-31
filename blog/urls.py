190d1ef5b9162dbeb6e673a4e4d2f5ad523b7fd9
aufcl731 <aufcl731@gmail.com>
2021-05-31 11:38:03 +0900

    [add] test

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main, name="main"),
-    url(r'^blog/(?P<pk>\d+)/$', views.post_detail, name="detail") #추가
+    url(r'^blog/(?P<pk>\d+)/$', views.post_detail, name="detail"), #추가
+    url(r'^post_list/$', views.post_list, name="post_list"),
     #pk: 인자
     #d+: 숫자
 
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, redirect
-from blog.models import *
+from django.utils import timezone
+from blog.models import Post
 
 def main(request):
     msg = {'msg': request}
-    return render(request, 'html/main.html', msg)
+    return render(request, 'html/main.html', {})
 
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'html/detail.html', {'posts': post})
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    list2 = Post.objects.filter(title__contains='title')
+    print(posts)
+    print(list2)
+    return render(request, 'html/post_list.html', {'posts': posts, 'list2': list2})
+
+
+
+
